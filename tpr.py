c2871bb7c0437534103b0e596cb0c7c5ab9d10ae
Cameron <lecourtcam2gmail.com>
2022-03-13 16:34:48 +0300

    add tpr.py

---
+++ tpr.py
@@ -0,0 +1,9 @@
+import math
+
+n = int(input("Введите число: "))
+count = 0
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if math.cos(i ** 2 + n) > 0:
+            count += 1
+print(count)
